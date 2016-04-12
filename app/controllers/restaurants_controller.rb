9ba274ef6693aac39b93d633b4a0709095ff4e2a
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-04-12 19:28:14 +0100

    Associating restaurants and reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,18 +7,35 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
-  def create
-    Restaurant.create(restaurant_params)
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
     redirect_to '/restaurants'
   end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
-
 
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many :reviews }
+
+  describe 'deleting a parent restaurant' do
+
+    # before do
+    #   Restaurant.create(name: 'restaurant')
+    # end
+    #
+    # it 'also deletes child reviews' do
+    #   puts "puts @restaurant:  #{@restaurant.first}"
+    # end
+  end
 end
