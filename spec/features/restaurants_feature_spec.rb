76240bd83c2d118e5a2b066b82e002ad2e0263a6
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-04-12 14:23:32 +0100

    Rendering restaurants in the view

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants should have been added' do
+
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
-      expect(page).to have_content('No restaurants yet')
-      expect(page).to have_link('Add a restaurant')
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
     end
   end
 end
